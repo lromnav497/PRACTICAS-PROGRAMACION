480b1a90789db2a621b198d7e888e6a2e9953063
lromnav497 <146075035+lromnav497@users.noreply.github.com>
2023-11-27 22:50:11 +0100

    .

--- src/unidad2Bucles/Practica9Unidad2Bucles.java
+++ src/unidad2Bucles/Practica9Unidad2Bucles.java
@@ -1,14 +1,59 @@
 package unidad2Bucles;
 
 public class Practica9Unidad2Bucles {
+	// Definir las constantes para los tipos de elementos
+    public static final char CIUDAD = 'T';
+    public static final char CAMINO = 'R';
+    public static final char MONSTRUO = 'M';
+    public static final char TIERRA_MALDITA = 'D';
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x = 8;
-		while (x > 0) {
-			System.out.println(x);
-			x -= 3;
-		}
-	}
+    public static void main(String[] args) {
+    	// Representación del mapa
+        char[][] mapa = {
+            {CIUDAD, CAMINO, CAMINO, CIUDAD, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, MONSTRUO},
+            {TIERRA_MALDITA, TIERRA_MALDITA, CAMINO, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, MONSTRUO, TIERRA_MALDITA},
+            {TIERRA_MALDITA, TIERRA_MALDITA, CAMINO, TIERRA_MALDITA, MONSTRUO, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA},
+            {MONSTRUO, TIERRA_MALDITA, CAMINO, CAMINO, CAMINO, CAMINO, TIERRA_MALDITA, TIERRA_MALDITA},
+            {TIERRA_MALDITA, MONSTRUO, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, CAMINO, TIERRA_MALDITA, TIERRA_MALDITA},
+            {TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, MONSTRUO, TIERRA_MALDITA, CAMINO, TIERRA_MALDITA, MONSTRUO},
+            {TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, CAMINO, CAMINO, CIUDAD},
+            {MONSTRUO, MONSTRUO, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA, TIERRA_MALDITA}
+        };
 
+        // Contadores para las ciudades, monstruos y caminos
+        int ciudades = 0;
+        int monstruos = 0;
+        int caminos = 0;
+
+        // Contar las ciudades, monstruos y caminos
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[i].length; j++) {
+                switch (mapa[i][j]) {
+                    case CIUDAD:
+                        ciudades++;
+                        break;
+                    case MONSTRUO:
+                        monstruos++;
+                        break;
+                    case CAMINO:
+                        caminos++;
+                        break;
+                }
+            }
+        }
+
+        // Comprobar si las ciudades están conectadas por caminos
+        if (caminos >= ciudades - 1) {
+            System.out.println("Las ciudades están conectadas por caminos.");
+        } else {
+            System.out.println("Las ciudades no están conectadas por caminos.");
+        }
+
+        // Comprobar si hay más monstruos que ciudades
+        if (monstruos > ciudades) {
+            System.out.println("No hay esperanza, hay más monstruos que ciudades.");
+        } else {
+            System.out.println("Hay esperanza, hay más ciudades que monstruos.");
+        }
+    }
 }
